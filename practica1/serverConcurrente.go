1b8b0d59894a951deacf12454620cb85547a571e
Aaron Ibañez Espes <aaronainzon@gmail.com>
2021-10-07 16:34:25 +0200

    mod

---
+++ practica1/serverConcurrente.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+	"fmt"
+	"net"
+	"os"
+	"p1/com"
+)
+
+func checkError(err error) {
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Fatal error: %s", err.Error())
+		os.Exit(1)
+	}
+}
+
+// PRE: verdad
+// POST: IsPrime devuelve verdad si n es primo y falso en caso contrario
+func IsPrime(n int) (foundDivisor bool) {
+	foundDivisor = false
+	for i := 2; (i < n) && !foundDivisor; i++ {
+		foundDivisor = (n%i == 0)
+	}
+	return !foundDivisor
+}
+
+// PRE: interval.A < interval.B
+// POST: FindPrimes devuelve todos los números primos comprendidos en el
+// 		intervalo [interval.A, interval.B]
+func FindPrimes(interval com.TPInterval) (primes []int) {
+	for i := interval.A; i <= interval.B; i++ {
+		if IsPrime(i) {
+			primes = append(primes, i)
+		}
+	}
+	return primes
+}
+
+func main() {
+
+	listener, err := net.Listen(CONN_TYPE, CONN_HOST+":"+CONN_PORT)
+	checkError(err)
+
+	conn, err := listener.Accept()
+	defer conn.Close()
+	checkError(err)
+
+	// TO DO
+}
