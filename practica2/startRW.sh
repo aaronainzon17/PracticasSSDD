61ed5740ab8c30e1690e2f1f30e6fbc1214a34e0
Aaron Ibañez Espes <aaronainzon@gmail.com>
2021-10-28 22:00:26 +0200

    FIN

--- practica2/startRW.sh
+++ practica2/startRW.sh
@@ -6,17 +6,16 @@ if [ $# -ne 1 ]; then
 fi
 echo $0 $1
 
-go run gestorfichero.go 5 ./ms/users.txt &
+go run gestorfichero.go 4 ./ms/users.txt &
+
 if [ $1=="GoVec" ]; then 
-    go run escritorGoVec.go 1 4 ./ms/users.txt &
-    go run escritorGoVec.go 2 4 ./ms/users.txt &
-    go run escritorGoVec.go 3 4 ./ms/users.txt &
-    go run escritorGoVec.go 4 4 ./ms/users.txt &
+    go run lectorGoVec.go 1 3 ./ms/users.txt &
+    go run lectorGoVec.go 2 3 ./ms/users.txt &
+    go run escritorGoVec.go 3 3 ./ms/users.txt &
 else 
-    go run lector.go 1 4 ./ms/users.txt &
-    go run lector.go 2 4 ./ms/users.txt &
-    go run lector.go 3 4 ./ms/users.txt &
-    go run lector.go 4 4 ./ms/users.txt &
+    go run lector.go 1 3 ./ms/users.txt &
+    go run lector.go 2 3 ./ms/users.txt &
+    go run escritor.go 3 3 ./ms/users.txt &
 fi
 
 exit 0
